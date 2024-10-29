be91b0c2af22077efc9163d09cf3e745613d45a5
Eduardo Iniesta <einiesta@moval.es>
2024-10-29 12:53:31 +0100

    [START] foa: repository creation.

---
+++ base_foa/__manifest__.py
@@ -0,0 +1,19 @@
+# 2024 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Forestry Association Management",
+    "summary": "In a forestry assocation, management of "
+               "owners and parcels.",
+    "version": "16.0.1.0.0",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "website": "https://moval.es",
+    "category": "Forestry Associations",
+    "depends": [
+        'base_ter',
+    ],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+    "application": False,
+}
